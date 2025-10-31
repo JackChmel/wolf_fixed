67f879a1a1385810e2c4c7285ae73fef6f9cf778
JackChmel <jack.chmel@gmail.com>
2025-10-31 22:29:33 +0100

    Update WolfChunkLoaderTickets.java

--- wolfchunk_loader/src/main/java/com/example/wolfchunkloader/WolfChunkLoaderTickets.java
+++ wolfchunk_loader/src/main/java/com/example/wolfchunkloader/WolfChunkLoaderTickets.java
@@ -3,7 +3,14 @@ package com.example.wolfchunkloader;
 import net.minecraft.server.level.TicketType;
 import net.minecraft.world.level.ChunkPos;
 
+import java.util.Comparator;
+
 public class WolfChunkLoaderTickets {
+
     public static final TicketType<ChunkPos> ENTITY_TICKING =
-            TicketType.create("wolf_entity_ticking", ChunkPos::compareTo);
+            TicketType.create(
+                    "wolf_entity_ticking",
+                    Comparator.comparingInt((ChunkPos pos) -> pos.x)
+                              .thenComparingInt(pos -> pos.z)
+            );
 }
